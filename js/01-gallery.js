b93998299aa174f24404c50f9ae4eea645ce6c23
Ruslan <Giovingo92@gmail.com>
2023-03-02 22:33:47 +0200

    add first homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const containerGalleryRef = document.querySelector(".gallery");
+
+const handleCreateImgElements = (galleryItems) => {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+    </div>`;
+    })
+    .join("");
+};
+
+containerGalleryRef.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  handleShowImgElement(e.target.dataset.source);
+});
+
+const handleShowImgElement = (e) => {
+  const instance = basicLightbox.create(`
+    <img src="${e}">
+`);
+  instance.show();
+
+  containerGalleryRef.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+};
+
+containerGalleryRef.innerHTML = handleCreateImgElements(galleryItems);
